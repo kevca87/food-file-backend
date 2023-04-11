bbaa3f0e6cfbc64b5f3a75ee00a769019db505da
Kevin <kevcespedesarancibia87@gmail.com>
2023-04-11 19:39:21 -0400

    Added harcoded recipes

--- food-file-backend/Controllers/RecipesController.cs
+++ food-file-backend/Controllers/RecipesController.cs
@@ -11,7 +11,10 @@ namespace food_file_backend.Controllers
         {
             _recipes = new List<RecipeModel>();
             _recipes.Add(new RecipeModel() { Id=1, Name="Pique Macho", Country="Bolivia", Description="El mejor plato de Bolivia" });
-            _recipes.Add(new RecipeModel() { Id=2, Name="Silpancho", Country="Bolivia", Description="El mejor plato de Bolivia" });
+            _recipes.Add(new RecipeModel() { Id=2, Name="Silpancho", Country="Bolivia", Description="Carne delgada y apanada con papas retostadas" });
+            _recipes.Add(new RecipeModel() { Id=3, Name="Relleno de papa", Country="Bolivia", Description="Un clasico de Cochabamba." });
+            _recipes.Add(new RecipeModel() { Id=4, Name="Salchipapa", Country="Bolivia", Description="Plato rapido de elaborar, con pocos ingredientes." });
+            _recipes.Add(new RecipeModel() { Id=5, Name="Spaggetti", Country="Italia", Description="Plato de pasta" });
         }
 
         [HttpGet]
