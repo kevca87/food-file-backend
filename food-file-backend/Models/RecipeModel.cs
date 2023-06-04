ded8a544861b3f6cf174e10b91bc6ada4729c884
Micaela Patiño <micaela.patino@ucb.edu.bo>
2023-06-04 13:44:41 -0400

    Recipe model modified

--- food-file-backend/Models/RecipeModel.cs
+++ food-file-backend/Models/RecipeModel.cs
@@ -3,7 +3,7 @@
     public class RecipeModel
     {
         public int Id { get; set; }
-        public string? RecipeName { get; set; }
+        public string? Name { get; set; }
         public string? Instructions { get; set; }
         public string? Country { get; set; }
         public string? Difficulty { get; set; }
