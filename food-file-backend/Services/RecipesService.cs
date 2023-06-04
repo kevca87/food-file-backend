b1d64b120c9d6ed9ab0e923b42561cf24a32bb0a
Micaela Patiño <micaela.patino@ucb.edu.bo>
2023-06-04 13:48:28 -0400

    Recipe model modified

--- food-file-backend/Services/RecipesService.cs
+++ food-file-backend/Services/RecipesService.cs
@@ -8,11 +8,11 @@ namespace food_file_backend.Services
         public RecipesService()
         {
             _recipes = new List<RecipeModel>();
-            _recipes.Add(new RecipeModel() { Id = 1, RecipeName = "Pique Macho", Country = "Bolivia", Description = "El mejor plato de Bolivia" });
-            _recipes.Add(new RecipeModel() { Id = 2, RecipeName = "Silpancho", Country = "Bolivia", Description = "Carne delgada y apanada con papas retostadas" });
-            _recipes.Add(new RecipeModel() { Id = 3, RecipeName = "Relleno de papa", Country = "Bolivia", Description = "Un clasico de Cochabamba." });
-            _recipes.Add(new RecipeModel() { Id = 4, RecipeName = "Salchipapa", Country = "Bolivia", Description = "Plato rapido de elaborar, con pocos ingredientes." });
-            _recipes.Add(new RecipeModel() { Id = 5, RecipeName = "Spaggetti", Country = "Italia", Description = "Plato de pasta" });
+            _recipes.Add(new RecipeModel() { Id = 1, Name = "Pique Macho", Country = "Bolivia", Description = "El mejor plato de Bolivia" });
+            _recipes.Add(new RecipeModel() { Id = 2, Name = "Silpancho", Country = "Bolivia", Description = "Carne delgada y apanada con papas retostadas" });
+            _recipes.Add(new RecipeModel() { Id = 3, Name = "Relleno de papa", Country = "Bolivia", Description = "Un clasico de Cochabamba." });
+            _recipes.Add(new RecipeModel() { Id = 4, Name = "Salchipapa", Country = "Bolivia", Description = "Plato rapido de elaborar, con pocos ingredientes." });
+            _recipes.Add(new RecipeModel() { Id = 5, Name = "Spaggetti", Country = "Italia", Description = "Plato de pasta" });
 
         }
         public async Task<RecipeModel> CreateRecipe(RecipeModel model)
